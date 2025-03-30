7156ce3740e164e8d05576b717c8c54dea666109
Program8 <appsgem123@gmail.com>
2025-03-30 12:02:02 +0530

    Added method add

---
+++ Sources/SwiftPackagePublishingDemo/MyCalculator.swift
@@ -0,0 +1,14 @@
+//
+//  MyCalculator.swift
+//  SwiftPackagePublishingDemo
+//
+//  Created by Vijay Sachan on 30/03/25.
+//
+
+public class MyCalculator {
+    public init() {}
+    /// Added in 1.0.0
+    public func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+}
